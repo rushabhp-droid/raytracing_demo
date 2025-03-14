7d72213b983ea03169093c03efa1258c77115e21
Rushabh Patil <rushabhpatil@tutamail.com>
2025-03-14 11:13:25 +0530

    comments

--- main.c
+++ main.c
@@ -7,10 +7,10 @@
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("raytracing demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
-    SDL_Surface* surface = SDL_GetWindowSurface(window);
-    SDL_Rect rect = (SDL_Rect) {200,200,200,200};
-    SDL_FillRect(surface, &rect, COLOR_WHITE);
-    SDL_UpdateWindowSurface(window);
+    SDL_Window* window = SDL_CreateWindow("raytracing demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0); // icreate window
+    SDL_Surface* surface = SDL_GetWindowSurface(window); //
+    SDL_Rect rect = (SDL_Rect) {200,200,200,200}; // create a rectangle shape in the window
+    SDL_FillRect(surface, &rect, COLOR_WHITE); // :)
+    SDL_UpdateWindowSurface(window); // Update window to display rectangle
     SDL_Delay(6000);
 }
