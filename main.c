259a803426266c93403ddc27a26b5361510165d6
Rushabh Patil <rushabhpatil@tutamail.com>
2025-03-14 11:06:25 +0530

    basic window creation

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+#define WIDTH 900
+#define HEIGHT 800
+#define COLOR_WHITE 0xffffffff
+
+int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("raytracing demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+    SDL_Rect rect = (SDL_Rect) {200,200,200,200};
+    SDL_FillRect(surface, &rect, COLOR_WHITE);
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(6000);
+}
